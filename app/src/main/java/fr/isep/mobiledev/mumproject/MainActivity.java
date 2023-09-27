71bfb4ad96371956cd00381d3aa1f9c1fced8ffb
Nathan WEBER <nathan.weber@eleve.isep.fr>
2023-09-27 11:28:31 +0200

    Call button implemented

--- app/src/main/java/fr/isep/mobiledev/mumproject/MainActivity.java
+++ app/src/main/java/fr/isep/mobiledev/mumproject/MainActivity.java
@@ -2,8 +2,12 @@ package fr.isep.mobiledev.mumproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageResource(R.drawable.placeholder_1);
+
+        Button buttonCall = findViewById(R.id.callButton);
+        buttonCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:+33422521010"));
+                startActivity(intent);
+            }
+        });
     }
 }
