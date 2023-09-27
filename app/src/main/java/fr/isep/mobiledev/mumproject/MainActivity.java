de2aadbd685e9981a14697ee301aeb7037036636
Florentin Leclercq <github@email.leclercq-florentin.fr>
2023-09-27 11:06:49 +0200

    Added first activity

--- app/src/main/java/fr/isep/mobiledev/mumproject/MainActivity.java
+++ app/src/main/java/fr/isep/mobiledev/mumproject/MainActivity.java
@@ -2,7 +2,9 @@ package fr.isep.mobiledev.mumproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.placeholder_1);
     }
 }
