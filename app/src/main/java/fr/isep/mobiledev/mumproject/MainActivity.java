612c7ef6e7dc555c2c1518128fc60e700a5e9cc6
Florentin Leclercq <github@email.leclercq-florentin.fr>
2023-09-27 11:50:54 +0200

    Added fade background image

--- app/src/main/java/fr/isep/mobiledev/mumproject/MainActivity.java
+++ app/src/main/java/fr/isep/mobiledev/mumproject/MainActivity.java
@@ -2,11 +2,25 @@ package fr.isep.mobiledev.mumproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TypedArray images;
+    private List<Integer> imageOrder;
+    private int imageIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +29,54 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageResource(R.drawable.placeholder_1);
+
+        images = getResources().obtainTypedArray(R.array.mum_images);
+        imageOrder = IntStream.rangeClosed(0, images.length()-1).boxed().collect(Collectors.toList());
+        Collections.shuffle(imageOrder);
+
+        imageView.setImageResource(getRandomImage());
+
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        Animation fadeOut = AnimationUtils.loadAnimation(this, R.anim.fadeout);
+
+        imageView.startAnimation(fadeIn);
+
+        fadeIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                final Handler handler = new Handler(Looper.getMainLooper());
+                handler.postDelayed(() -> {
+                    imageView.startAnimation(fadeOut);
+                }, 4000);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.setImageResource(getRandomImage());
+                imageView.startAnimation(fadeIn);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+    }
+
+    public int getRandomImage() {
+        if (imageIndex == images.length()) {
+            imageIndex = 0;
+            Collections.shuffle(imageOrder);
+        }
+        return images.getResourceId(imageOrder.get(imageIndex++), R.drawable.ic_launcher_background);
     }
 }
